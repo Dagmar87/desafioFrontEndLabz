76a18f1920ba0a37828216e396b6e43cfea4bc20
Dagmar87 <jdfssobrinho@hotmail.com>
2024-09-10 15:57:45 -0300

    Desenvolvimento do UserComponent e dos arquivos redux da pasta chat-client-frontend.

---
+++ chat-client-frontend/src/components/UserComponent.tsx
@@ -0,0 +1,39 @@
+import { setUser } from '@/redux/userReducer';
+import { Button, Input } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+const UserComponent = () => {
+  const [username, setUsername] = useState<string>();
+
+  const dispatch = useDispatch();
+
+  const updateUsername = () => {
+    dispatch(setUser(username));
+  };
+
+  return (
+    <div className="w-1/2 h-[70vh] border bg-white rounded-xl flex flex-col items-center justify-center">
+      <div className="w-1/2">
+        <div className="p-3 px-4 flex gap-4">
+          <Input
+            placeholder="username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <Button
+            colorScheme="teal"
+            onClick={() => {
+              updateUsername();
+            }}
+          >
+            Participar
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserComponent;
